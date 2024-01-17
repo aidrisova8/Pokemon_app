b6f9354e800bc0a5a6d9493b94c172d6fecc1ef7
sdetai <aigulidrisova8@gmail.com>
2024-01-16 19:29:27 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/pokemon',(req,res)=>{
     res.render('Index',{pokemon:pokemon})
 })
 
+app.get('/pokemon/:id',(req,res)=>{
+    res.send(req.params.id)
+})
+
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT)
    
